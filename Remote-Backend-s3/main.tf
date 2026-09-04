1971a09cc0e2fc9ad621418f90d17cbc08364832
Htet Pai <zzzhtetpai@gmail.com>
2026-09-04 17:02:33 +0900

    feat: add S3 remote backend and DynamoDB state locking

---
+++ Remote-Backend-s3/main.tf
@@ -0,0 +1,53 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "htetpai-terraform-state-bucket"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+# versioning configuration for the S3 bucket
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id            
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "htetpai-terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name       = "LockID"
+    type       = "S"   #stringtype
+  }
+}
+
+terraform {
+backend "s3" {
+    bucket     = "htetpai-terraform-state-bucket"
+    key         = "global/s3/terraform.tfstate"
+    region    = "us-west-2"
+    dynamodb_table = "htetpai-terraform-state-locks"
+    encrypt      = true
+  }
+}
